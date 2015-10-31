f50807446578d489fa76810ab8c0e030ce084f58
jsfeb26 <jsfeb26@gmail.com>
2015-10-31 12:29:08 -0400

    Update folder structure
    Use client and server folders to control what code runs where

---
+++ client/addPlayerEvents.js
@@ -0,0 +1,12 @@
+Meteor.subscribe('thePlayers');
+
+Template.addPlayerForm.events({
+  'submit form': function(event) {
+    event.preventDefault();
+
+    var playerNameVar = event.target.playerName.value;
+    Meteor.call('insertPlayerData', playerNameVar);
+
+    event.target.playerName.value = "";
+  }
+});
---
+++ collection.js
@@ -0,0 +1,2 @@
+// by not using the var keyword the variable PlayerList is in the global scope
+PlayerList = new Mongo.Collection('players');
